176138ec3630f6d9733576990d0e4dca88c69de8
gbroccol <pchelinanst@gmail.com>
2023-11-01 21:01:32 +0100

    Move files

--- 10_Activity/app/src/main/java/ru/otus/otusdemo/ActivityA.kt
+++ 10_Activity/app/src/main/java/ru/otus/otusdemo/ActivityA.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 
+
 class ActivityA : AppCompatActivity() {
 
     private val buttonOpenActivityB by lazy { findViewById<Button>(R.id.buttonOpenActivityB) }
